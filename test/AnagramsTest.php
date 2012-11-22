163c1fefebe4e9dc18d2a09830198eb05125bb86
Pepe Doval <pepellou@gmail.com>
2012-11-22 17:08:45 +0100

    Subtle fixes

--- test/AnagramsTest.php
+++ test/AnagramsTest.php
@@ -8,21 +8,31 @@ class AnagramsTest extends PHPUnit_Framework_TestCase {
 
 	public function setUp(
 	) {
-		$this->anagrams = new Anagrams(dirname(__FILE__)."/../english-words.95");
+		$this->anagrams = new Anagrams(dirname(__FILE__)."/../english-words.96");
 	}
 
-/*
+	/* ACCEPTANCE TESTS */
+
 	public function test_we_get_array_of_groups(
 	) {
 		$this->assertTrue(is_array($this->anagrams->getGroups()));
 	}
 
-*/
+	public function test_we_get_known_result(
+	) {
+		$result = $this->anagrams->getGroups();
+		$this->assertTrue(
+			in_array("rots sort", $result)
+		);
+	}
+
+	/* ENDOF ACCEPTANCE TESTS */
+
 	public function test_we_store_the_input_lines(
 	) {
 		$this->assertTrue(is_array($this->anagrams->input_lines));
 		$this->assertEquals(
-			219156,
+			17,
 			count($this->anagrams->input_lines)
 		);
 	}
@@ -67,14 +77,6 @@ class AnagramsTest extends PHPUnit_Framework_TestCase {
 			array(1, "sort", array("a", "rots")),
 			array(2, "sort", array("a", "b", "rots")),
 			array(2, "sort", array("a", "b", "rots sort")),
-		);
-	}
-
-	public function test_we_get_known_result(
-	) {
-		$result = $this->anagrams->getGroups();
-		$this->assertTrue(
-			in_array("rots sort", $result)
 		);
 	}
 
