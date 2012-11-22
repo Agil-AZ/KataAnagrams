8b1e24befd1bf5f3e1e67137c1c48ae80874bf43
Pepe Doval <pepellou@gmail.com>
2012-11-22 14:32:28 +0100

    First try

--- src/Anagrams.php
+++ src/Anagrams.php
@@ -2,4 +2,67 @@
 
 class Anagrams {
 
+	public $input_lines;
+
+	public function __construct(
+		$fileName
+	) {
+		$this->input_lines = file($fileName);
+	}
+
+	public function getGroups(
+	) {
+		$groups = array();
+		foreach ($this->input_lines as $word) {
+			$numGroup = self::findGroup($word, $groups);
+			if ($numGroup == -1) {
+				$groups []= $word;
+			} else {
+				$groups[$numGroup] .= " ".$word;
+			}
+		}
+		return $groups;
+	}
+
+	public static function findGroup(
+		$word,
+		$groups
+	) {
+		for ($index = 0; $index < count($groups); $index++) {
+			$group = $groups[$index];
+			if (self::wordIsInGroup($word, $group)) {
+				return $index;
+			}
+		}
+		return -1;
+	}
+
+	public static function wordIsInGroup(
+		$word,
+		$group
+	) {
+		$wordsInGroup = explode(" ", $group);
+		foreach ($wordsInGroup as $existingWord) {
+			if (self::isAnagram($word, $existingWord)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function isAnagram(
+		$aWord,
+		$anotherWord
+	) {
+		if (strlen($aWord) != strlen($anotherWord)) {
+			return false;
+		}
+		for ($i = 0; $i < strlen($aWord); $i++) {
+			$theChar = $aWord[$i];
+			if (strpos($anotherWord, $theChar) === false)
+				return false;
+		}
+		return true;
+	}
+
 }
--- test/AnagramsTest.php
+++ test/AnagramsTest.php
@@ -4,15 +4,78 @@ require_once (dirname(__FILE__)."/../src/Anagrams.php");
 
 class AnagramsTest extends PHPUnit_Framework_TestCase {
 
-	public function test_sample(
+	private $anagrams;
+
+	public function setUp(
 	) {
-		$this->fail("Ouch!");
-		/* OTHER USEFUL ASSERTS:
-			$this->assertEquals($expectedValue, $whatever);
-			$this->assertNotEquals($notExpectedValue, $whatever);
-			$this->assertTrue($whatever);
-			$this->assertFalse($whatever);
-		*/
+		$this->anagrams = new Anagrams(dirname(__FILE__)."/../english-words.95");
+	}
+
+/*
+	public function test_we_get_array_of_groups(
+	) {
+		$this->assertTrue(is_array($this->anagrams->getGroups()));
+	}
+
+*/
+	public function test_we_store_the_input_lines(
+	) {
+		$this->assertTrue(is_array($this->anagrams->input_lines));
+		$this->assertEquals(
+			219156,
+			count($this->anagrams->input_lines)
+		);
+	}
+
+	public function test_word_is_anagram(
+	) {
+		$this->assertTrue(Anagrams::isAnagram("sort", "sort"));
+		$this->assertTrue(Anagrams::isAnagram("sort", "rots"));
+		$this->assertFalse(Anagrams::isAnagram("sort", "ots"));
+		$this->assertFalse(Anagrams::isAnagram("sort", "a"));
+	}
+
+	public function test_word_is_in_group(
+	) {
+		$this->assertTrue(Anagrams::wordIsInGroup("sort", "a b sort"));
+		$this->assertTrue(Anagrams::wordIsInGroup("sort", "sort"));
+		$this->assertTrue(Anagrams::wordIsInGroup("sort", "rots sort"));
+		$this->assertFalse(Anagrams::wordIsInGroup("sort", "ots sot"));
+		$this->assertFalse(Anagrams::wordIsInGroup("sort", "a"));
+	}
+
+	/**
+	* @dataProvider groups_and_words
+	*/
+	public function test_find_group(
+		$expectedIndex,
+		$word,
+		$groups
+	) {
+		$this->assertEquals(
+			$expectedIndex,
+			Anagrams::findGroup($word, $groups)
+		);
+	}
+ 
+	public static function groups_and_words(
+	) {
+		return array(
+			array(-1, "sort", array("a")),
+			array(-1, "sort", array("a", "rts")),
+			array(0, "sort", array("rots")),
+			array(1, "sort", array("a", "rots")),
+			array(2, "sort", array("a", "b", "rots")),
+			array(2, "sort", array("a", "b", "rots sort")),
+		);
+	}
+
+	public function test_we_get_known_result(
+	) {
+		$result = $this->anagrams->getGroups();
+		$this->assertTrue(
+			in_array("rots sort", $result)
+		);
 	}
 
 }
