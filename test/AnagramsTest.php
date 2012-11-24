48ffe5646033a44b763e4ed71888a8e983bf3d80
Pepe Doval <pepellou@gmail.com>
2012-11-24 15:19:57 +0100

    Testing all input files

--- test/AnagramsTest.php
+++ test/AnagramsTest.php
@@ -4,37 +4,55 @@ require_once (dirname(__FILE__)."/../src/Anagrams.php");
 
 class AnagramsTest extends PHPUnit_Framework_TestCase {
 
-	private $anagrams;
-
-	public function setUp(
-	) {
-		$this->anagrams = new Anagrams(dirname(__FILE__)."/../medium.txt");
-	}
-
 	/* ACCEPTANCE TESTS */
 
+	/**
+	* @dataProvider input_files
+	*/
 	public function test_we_get_array_of_groups(
+		$theFile
 	) {
-		$this->assertTrue(is_array($this->anagrams->getGroups()));
+		$anagrams = new Anagrams($theFile);
+		$this->assertTrue(is_array($anagrams->getGroups()));
 	}
 
+	/**
+	* @dataProvider input_files
+	*/
 	public function test_we_get_known_result(
+		$theFile
 	) {
-		$result = $this->anagrams->getGroups();
+		$anagrams = new Anagrams($theFile);
+		$result = $anagrams->getGroups();
 		$this->assertEquals(
-			array("rots", "sort"), 
+			array("rost", "rots", "sort"), 
 			$result["orst"]
 		);
 	}
 
 	/* ENDOF ACCEPTANCE TESTS */
 
+	/**
+	* @dataProvider input_files
+	*/
 	public function test_we_store_the_input_lines(
+		$theFile,
+		$theLines
 	) {
-		$this->assertTrue(is_array($this->anagrams->input_lines));
+		$anagrams = new Anagrams($theFile);
+		$this->assertTrue(is_array($anagrams->input_lines));
 		$this->assertEquals(
-			250,
-			count($this->anagrams->input_lines)
+			$theLines,
+			count($anagrams->input_lines)
+		);
+	}
+
+	public static function input_files(
+	) {
+		return array(
+			array(dirname(__FILE__)."/../small.txt",  18),
+			array(dirname(__FILE__)."/../medium.txt", 251),
+			array(dirname(__FILE__)."/../large.txt",  219156),
 		);
 	}
 
