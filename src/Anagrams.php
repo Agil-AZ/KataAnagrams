66b0b9561da3c13287e84fea0160b77cc3de114b
Pepe Doval <pepellou@gmail.com>
2012-11-24 14:59:24 +0100

    Optimization attempt on sorting chars
    Sorting chars within a string in PHP has the little problem that
    a string is not an array, and we end up setting up 2 or 3 loops
    to transform into array, sort and transform back again into
    string. Of course this is inneficient and we'll probably must
    consider modeling a word as an array of chars and reading that
    directly from the input.
    
    But in the meanwhile, we've made an attempt to improve sorting
    based in the fact that chars in a word belong to a very little
    set of values. Thus, we applied the 'cubes' sorting method instead
    of php arrays "sort" one. The 'cubes' algorithm is O(N) since it
    takes the time of "reading" the N elements, but since the words
    aren't very long we don't see an improvement in performance.

--- src/Anagrams.php
+++ src/Anagrams.php
@@ -59,6 +59,7 @@ class Word {
 
 	private $theWord;
 	private $canonical;
+	public static $zero_histogram;
 
 	public function __construct(
 		$theWord
@@ -81,16 +82,36 @@ class Word {
 	private function orderChars(
 	) {
 		$wLength = strlen($this->theWord);
-		$theChars = array();
+		$histogram = self::$zero_histogram;
 		for ($i = 0; $i < $wLength; $i++) {
-			$theChars []= $this->theWord[$i];
+			$theChar = $this->theWord[$i];
+			if (isset($histogram[$theChar])) {
+				$histogram[$theChar]++;
+			}
 		}
-		sort($theChars);
-		$theResult = "";
-		foreach ($theChars as $aChar) {
-			$theResult .= $aChar;
+		$result = "";
+		foreach ($histogram as $char => $count) {
+			for (; $count > 0; $count--) {
+				$result .= $char;
+			}
 		}
-		return $theResult;
+		return $result;
 	}
 
 }
+
+Word::$zero_histogram = array(
+	'\'' => 0, '-' => 0,
+	'A' => 0, 'B' => 0, 'C' => 0, 'D' => 0, 'E' => 0, 
+	'F' => 0, 'G' => 0, 'H' => 0, 'I' => 0, 'J' => 0,
+	'K' => 0, 'L' => 0, 'M' => 0, 'N' => 0, 'O' => 0, 
+	'P' => 0, 'Q' => 0, 'R' => 0, 'S' => 0, 'T' => 0,
+	'U' => 0, 'V' => 0, 'W' => 0, 'X' => 0, 'Y' => 0, 'Z' => 0, 
+	'a' => 0, 'b' => 0, 'c' => 0, 'd' => 0, 'e' => 0, 
+	'f' => 0, 'g' => 0, 'h' => 0, 'i' => 0, 'j' => 0,
+	'k' => 0, 'l' => 0, 'm' => 0, 'n' => 0, 'o' => 0, 
+	'p' => 0, 'q' => 0, 'r' => 0, 's' => 0, 't' => 0,
+	'u' => 0, 'v' => 0, 'w' => 0, 'x' => 0, 'y' => 0, 'z' => 0, 
+	'Á' => 0, 'É' => 0, 'Í' => 0, 'Ó' => 0, 'Ú' => 0,
+	'á' => 0, 'é' => 0, 'í' => 0, 'ó' => 0, 'ú' => 0,
+);
