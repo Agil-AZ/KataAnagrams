b1ec2acaca71c988b74d13658269f862dec26d05
Pablo Vicente <vicapa99@gmail.com>
2012-12-02 18:43:21 +0000

    Anagram can print words from one file

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,8 +3,15 @@ require 'spec_helper'
 describe 'anagram' do
   context 'when no words' do
     it 'prints an empty string' do
-      anagram = Anagram.new()
+      anagram = Anagram.new('')
       anagram.print.should eql ""
+    end
+  end
+
+  context 'when has a file with words' do
+    it 'print this words' do
+      anagram = Anagram.new('./three_words.txt')
+      anagram.print.should eql "pablo\npepe\nedu\n"
     end
   end
 end
--- src/anagram.rb
+++ src/anagram.rb
@@ -1,5 +1,19 @@
 class Anagram
+  def initialize(file)
+    begin
+      file = File.new('./spec/' + file, 'r')
+      @words = Array.new
+      while(line = file.gets)
+        @words << line
+      end
+    rescue => err
+      puts 'error'
+    end
+  end
+
   def print
-    ""
+    result = ""
+    @words.each { |word| result +=word } if @words
+    result
   end
 end
